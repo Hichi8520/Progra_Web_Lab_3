88e8c834cf580f8fa7d3d54745e788e408998915
Luis <hichi8520@gmail.com>
2021-03-04 16:15:38 -0600

    Ejercicio input terminado

--- Entregables/lab3_name.js
+++ Entregables/lab3_name.js
@@ -2,13 +2,18 @@ var http = require('http');
 var url = require('url');
 
 http.createServer(function (req, res) {    
+    // la respuesta será en formato json
     res.writeHead(200, {'Content-Type': 'application/json'});
 
+    // verifica que la ruta sea la indicada
     if (req.url.startsWith('/hello/:')) {
+
+        // obtiene y devuelve el nombre especificado en la ruta por el usuario
         var arr = req.url.split(':');
         var result = arr[1];
         res.end(JSON.stringify({ hello: result }));
     }
+    // si ingresa a una ruta diferente a la indicada
     else {
         res.write("Ingresar a la ruta: /hello/:name");
         res.end();
--- Entregables/lab3_text.js
+++ Entregables/lab3_text.js
@@ -1,12 +1,18 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var bal = require('./balanced');
 
 http.createServer(function (req, res) {    
     res.writeHead(200, {'Content-Type': 'application/json'});
 
+    // al entrar al url básico
     if (req.url == '/') {
+
+        // abrir el doc html con el form para ingresar el texto
         fs.readFile('input.html', function(err, data) {
+
+            // si no encuentra el archivo html en el servidor
             if (err) {
                 res.writeHead(404, {'Content-Type': 'text/html'});
                 return res.end("404 Not Found");
@@ -16,12 +22,23 @@ http.createServer(function (req, res) {
             return res.end();
         });
     }
+    // al ingrsar el input y hacer clic en el botón se redirecciona a esta ruta
     else if (req.url.startsWith('/input/?finput=')) {
+
+        // se obtiene el texto ingresado
         var q = url.parse(req.url, true).query;
         var txt = q.finput;
 
-        res.end(txt);
+        // verificar el balance de llaves
+        if (bal.balancedFunc(txt)) {
+            res.end('Los paréntesis, llaves y corchetes están balanceados');
+        }
+        else {
+            res.end('Los paréntesis, llaves y corchetes NO están balanceados');
+        }
+        
     }
+    // si ingresa una ruta diferente 
     else {
         res.write("Ingresar a la ruta: /hello/:name");
         res.end();
